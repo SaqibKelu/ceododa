42ecec46f1074ae74bb73daf67cf0ade9dfb3470
Saqib Kelu <sakkikelu@gmail.com>
2024-02-08 15:17:15 +0530

    initial commit

---
+++ js/custom.js
@@ -0,0 +1,59 @@
+
+  (function ($) {
+  
+  "use strict";
+
+    // COUNTER NUMBERS
+    jQuery('.counter-thumb').appear(function() {
+      jQuery('.counter-number').countTo();
+    });
+    
+    // CUSTOM LINK
+    $('.smoothscroll').click(function(){
+    var el = $(this).attr('href');
+    var elWrapped = $(el);
+    var header_height = $('.navbar').height();
+
+    scrollToDiv(elWrapped,header_height);
+    return false;
+
+    function scrollToDiv(element,navheight){
+      var offset = element.offset();
+      var offsetTop = offset.top;
+      var totalScroll = offsetTop-navheight;
+
+      $('body,html').animate({
+      scrollTop: totalScroll
+      }, 300);
+    }
+});
+    
+  })(window.jQuery);
+
+
+/**owl carousel */
+  $(document).ready(function() {
+ 
+    $(".owl-carousel").owlCarousel({
+   
+        autoPlay: 3000,
+        items : 4,
+        itemsDesktop : [1199,3],
+        itemsDesktopSmall : [979,3],
+        center: true,
+        nav:true,
+        loop:true,
+        responsive: {
+          600: {
+            items: 4
+          }
+        }
+       
+       
+        
+        
+        
+   
+    });
+   
+  });
